d23e7eed93ac7af570013b62103d6edb62b2f544
Sabo05 <dariochen2005@gmail.com>
2024-11-06 19:31:30 +0100

    Comand menu', Some status_code, invio messaggio al server

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class Main {
 
     private static String username;
+    //private static String[] group_codes;
     
     public static void main(String[] args) throws UnknownHostException, IOException {
         Socket socket = new Socket("localhost", 3000);
@@ -25,7 +26,7 @@ public class Main {
         //Validazione della connessione tramite l'inserimento dell'username
         Input_username name = new Input_username(scanner, in, out);
         username = name.input_username();
-        System.out.println("Username: " + username);
+        System.out.println("\n Username: " + username);
 
         ReceiveThread r = new ReceiveThread(in, out);
         r.start();
@@ -42,13 +43,15 @@ public class Main {
     
     public static void sendMessage(BufferedReader in, DataOutputStream out, Scanner scan) throws IOException{
         String message;
+        //Stampa i comandi eseguibili dall'utente
+        print_comands();
         // Loop per inviare messaggi
         while (true) { 
-            System.out.println("Inserire il messaggio da mandare all'altro utente");
+            System.out.println("Enter the command to execute or the message you wish to send");
             message = scan.nextLine();
             // Invia il messaggio al server
             out.writeBytes(message + "\n");
-    
+
             // Condizione di uscita
             if (message.equalsIgnoreCase("exit")) {
                 System.out.println("Disconnessione...");
@@ -56,4 +59,29 @@ public class Main {
             }
         }
     }
+
+    public static void print_comands(){
+        //Sending a message
+        System.out.println("- - TO SEND A MESSAGE (no need for the \"\" when writing the message) - - ");
+        System.out.println("@nome_username \"message\" to send a message to user nome_username");
+        System.out.println("@All \"message\" to send a message to everyone");
+        System.out.println("G@group_name \"message\" to send a message to group group_name");
+        //Create group or add user
+        System.out.println("- - GROUP CREATION / USER FRIENDSHIP - - ");
+        System.out.println("/create_group \"group_name\" to create a group with group_name");
+        System.out.println("/add_user \"username\" to add user with username");
+        System.out.println("/accept Accept the last friendship request");
+        System.out.println("/accept_all Accept all friendship requests");
+        System.out.println("/reject Reject the last friendship request");
+        System.out.println("/reject_all Reject all friendship requests");
+        //Lists
+        System.out.println("- - LISTS - - ");
+        System.out.println("@_list show all available private chats");
+        System.out.println("G@_list show all available groupchats");
+        System.out.println("/list_all to show both the available private chats and groupchats");
+        System.out.println("Enter EXIT to exit");
+
+    }
+
+    //Simple Chat(Client - Server) Communication, hours spent coding: 8 
 }
--- src/main/java/com/example/ReceiveThread.java
+++ src/main/java/com/example/ReceiveThread.java
@@ -28,4 +28,39 @@ public class ReceiveThread extends Thread{
         }
     }
 
+    public void status_codes(String server_response){
+        try {
+            //Mancano ancora diversi messaggi di errore
+            switch(server_response){
+                case "ERROR_404":
+                    System.out.println("ERROR - - REQUEST NOT FOUND");
+                    break;
+                
+                case "ERROR_404_G":
+                    System.out.println("Group/s NOT FOUND");
+                    break;
+                
+                case "ERROR_404_P":
+                    System.out.println("Private Chat/s NOT FOUND");
+                    break;
+
+                case "ERROR_405":
+                    
+                    break;
+
+                case "ERROR_500":
+                    
+                    break;
+
+                default: 
+                    System.out.println("- - ERROR PROCESSING THE RESPONSE FROM THE SERVER - - ");
+                    break;
+
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("- - ERROR - - ");
+        }
+    }
+
 }
