cce4975392ff981f6473362d56e9d3931df5e47b
lai2-24 <lai2-24@itismeucci.com>
2024-11-12 12:42:08 +0100

    feature uscita dal gruppo

--- src/main/java/com/example/ReceiveThread.java
+++ src/main/java/com/example/ReceiveThread.java
@@ -44,64 +44,75 @@ public class ReceiveThread extends Thread{
             //Mancano ancora diversi messaggi di errore e risposta
             switch(server_response){
                 
-                case "RCV_100":
+                case "RCV_100": //ricezione de3l mesaggio (invio messaggio da parte di un altro utente)
                     messaggio = in.readLine();
                     System.out.println("Message " + messaggio);
                     break;
 
-                case "RCV_200":
+                case "RCV_200": //conferma creazione di un gruppo
                     //Read the group_name and the code to save it in a HashMap
                     String group_name = in.readLine();
                     String group_code = in.readLine();
 
                     //HashMap contenente il nome del gruppo e il suo relativo codice
                     this.group_codes.put(group_name, group_code);
-                    System.out.println("Group Name: " + group_name + " Group Code: " + group_code);
+                    //System.out.println("Group Name: " + group_name + " Group Code: " + group_code);
+                    System.out.println("Grupo creato. Nome: " + group_name);
                     break;
 
-                case "CL_200": 
+                case "RMV_200":
+                    //leggo nome e codice gruppo
+                    String grp_name = in.readLine();
+                    String grp_code = in.readLine();
+
+                    this.group_codes.remove(grp_name, grp_code); //rimozione
+                    System.out.println("Uscito dal gruppo: " + grp_name);
+
+                    break;    
+
+                case "CL_200": //messaggio di conferma per il gruppo creato
                     System.out.println("Group Created");
                     System.out.println("Enter /join_G@ group_name - username1, username2...... To add the users to the group");
                     break;
 
-                case "SUCC_200": 
+                case "SUCC_200": //Richiesta completata con successo
                     System.out.println("Richiesta completata con successo");
                     break;
                 
-                case "SRV_200":
+                case "SRV_200": //risposta del server
                     messaggio = in.readLine();
                     System.out.println(messaggio);
                     break;
 
-                case "SUCC_201":
+                case "SUCC_201": //conferma dell'invio del messaggio
                     System.out.println("Messaggio inviato con successo");
                     break;
 
-                case "MENU_200":
+                case "MENU_200": //menu
                     print_menu(in);
                     break;
 
-                case "GRP_INFO":
+                case "GRP_INFO": //vieni aggiunto in un gruppo
                     group_add(in);
                     break;
 
-                case "ERROR_400":
+                case "ERROR_400": //errore carattere non valido
                     System.out.println("Invalid Character present, RETRY");
                     break;
 
-                case "ERROR_403":
+                case "ERROR_403": //Utente già presente
                     System.out.println("Utente già presente");
                     break;
 
-                case "ERROR_404":
+                case "ERROR_404": //not found
                     System.out.println("- - ERROR - - REQUEST NOT FOUND");
                     break;
                 
-                case "ERROR_404_G":
+                case "ERROR_404_G": //gruppo not foud
                     System.out.println("Group NOT FOUND");
                     break;
                 
-                case "ERROR_404_P":
+                case "ERROR_404_P": //chat not foud
                     System.out.println("User NOT FOUND");
                     break;
 
